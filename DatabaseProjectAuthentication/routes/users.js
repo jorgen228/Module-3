217d281e32b2117843217df9d6c8020f515eedf1
jorgen228 <j_skjegstad@startmail.com>
2023-02-17 15:57:23 +0100

    Crated user admin view

--- DatabaseProjectAuthentication/routes/users.js
+++ DatabaseProjectAuthentication/routes/users.js
@@ -3,12 +3,36 @@ var router = express.Router();
 var db = require("../models");
 var UserService = require("../services/UserService");
 var userService = new UserService(db);
-const { canSeeUserDetails } = require("./authMiddlewares");
+const {
+  canSeeUserDetails,
+  canSeeUserList,
+  checkIfAuthorized,
+  isAdmin,
+} = require("./authMiddlewares");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+router.get("/", canSeeUserList, async function (req, res, next) {
+  const users = await userService.getAll();
+  res.render("users", { users: users });
+});
 
 router.get("/:userId", canSeeUserDetails, async function (req, res, next) {
   const user = await userService.getOne(req.params.userId);
   const username = req.user?.username ?? 0;
   res.render("userDetails", { user: user, username });
 });
+
+router.delete(
+  "/",
+  checkIfAuthorized,
+  isAdmin,
+  jsonParser,
+  async function (req, res, next) {
+    let id = req.body.id;
+    await userService.deleteUser(id);
+    res.end();
+  }
+);
 
 module.exports = router;
